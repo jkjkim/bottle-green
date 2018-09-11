c96ceba648ce1b211b2c5b9f6ffdc199a70aeda8
Junha Kim <junha.kim2003>
2018-09-10 18:54:31 -0700

    new pages

--- src/containers/aboutUs.js
+++ src/containers/aboutUs.js
@@ -3,33 +3,81 @@ import './aboutUs.css';
 
 const AboutUs = () => {
     return (
-<<<<<<< HEAD
         <div>
             <h1>About Us</h1>
 
             <h2>Mission Statement</h2>
             <div>Bottle Green is a nonprofit organization that strives to use the power of recycling to help children in need at Rainbow House. By donating bottles and cans to Rainbow House for bottle redemption, Bottle Green hopes to create a ripple effect in the community so that everyone can take the initiative to help the community around them. Bottle Green Through this app, Bottle Green hopes to modernize bottle recycling so that it is convenient and efficient for all participants while also producing far reaching benefits for our local community. </div>
 
+
+
+            <h2>Why Recycle?</h2>
+            <div>Recycling is a meaningful practice that can directly impact our community by:
+</div>
+            <ul>
+                <li>Reducing carbon emissions</li>
+                <li>Saving energy and money</li>
+                <li>Creating jobs</li>
+                <li>Preventing water and air pollution</li>
+                <li>Ensuring a healthy, sustainable future</li>
+            </ul>
+
             <hi>Put description of our members here</hi>
 
             <h2>Our Team</h2>
-            <div>Andrew Kim: ____</div>
+            
+            
+            <div>Andrew Kim:</div>
+            <ul>
+                <li>School: Irvine High School</li>
+                <li>Grade: 11</li>
+                <li>Hobbies: None</li>
+                <li>Interesting Fact: None</li>
+            </ul>
+
+            <div>Jae Hwan Lee:</div>
+            <ul>
+                <li>School: Northwood High School</li>
+                <li>Grade: 12</li>
+                <li>Hobbles: Photography, biking, and basketball</li>
+                <li>Interesting Fact: He likes yellow starbursts</li>
+            </ul>
+
+
+            <div>Junha Kim:</div>
+            <ul>
+                <li>School: Troy High School</li>
+                <li>Grade: 10</li>
+                <li>Hobbles: Sleeping</li>
+                <li>Interesting Fact: He moved schools every 2 years</li>
+            </ul>
+
+
+            <div>Jun Hee Ryu:</div>
+            <ul>
+                <li>School: Portola High School</li>
+                <li>Grade: 11</li>
+                <li>Hobbles: None</li>
+                <li>Interesting Fact: favorite ice cream flavor is cookies and cream</li>
+            </ul>
 
             
-=======
-        <div className="Sign-upTitle">
-            <div>Andrew Kim: ____</div>
-            <div>Andrew Kim: ____</div>
-            <div>Andrew Kim: ____</div>
-            <div>Andrew Kim: ____</div>
-            <div>Andrew Kim: ____</div>
-            <div className="aboutUs">Andrew Kim: ____</div>
-            <div className="Cause">
-                <div>Andrew Kim: ____</div>
-                <div>Andrew Kim: ____</div>
-                <div>Andrew Kim: ____</div>
-            </div>
->>>>>>> 394ee797248970964acf8edcd642ec89f51a3113
+            <div>Jimin Jung:</div>
+            <ul>
+                <li>School: Sunny Hills High School</li>
+                <li>Grade: 11</li>
+                <li>Hobbles: Basketball</li>
+                <li>Interesting Fact: He has “blonde” hair that he has had since birth</li>
+            </ul>
+
+
+            <div>Bryan Hwang:</div>
+            <ul>
+                <li>School: Beckman High School</li>
+                <li>Grade: 10</li>
+                <li>Hobbles: none</li>
+                <li>Interesting Fact: Favorite food is filet mignon</li>
+            </ul>
         </div>
     )
 }
--- src/containers/howto.js
+++ src/containers/howto.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 const HowTo = () => {
     return (
         <div>
-            <h1>How To</h1>
+            <h1>How To Recycle</h1>
+            
             
         </div>
     )
